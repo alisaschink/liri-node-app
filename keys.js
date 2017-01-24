d520da73677222784dae56ffbb3c40b4fbcc88b4
Alisa Schink <Alisa Schink>
2017-01-24 15:07:24 -0500

    access keys and secrets added

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<tni6iRfuZ8LJ4rqnInFWSobqY>',
-  consumer_secret: '<SPJuq9SqeDS03qOVloAZUdttQtu29aQJ7VpuDpWYTp3Pk8Q0ZZ>',
-  access_token_key: '<tni6iRfuZ8LJ4rqnInFWSobqY>',
-  access_token_secret: '< SPJuq9SqeDS03qOVloAZUdttQtu29aQJ7VpuDpWYTp3Pk8Q0ZZ>',
+  consumer_key: 'tni6iRfuZ8LJ4rqnInFWSobqY',
+  consumer_secret: 'SPJuq9SqeDS03qOVloAZUdttQtu29aQJ7VpuDpWYTp3Pk8Q0ZZ',
+  access_token_key: 'tni6iRfuZ8LJ4rqnInFWSobqY',
+  access_token_secret: 'SPJuq9SqeDS03qOVloAZUdttQtu29aQJ7VpuDpWYTp3Pk8Q0ZZ',
 }
