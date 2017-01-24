69bc69c4712445eb052cb604567d60462af3b656
Alisa Schink <Alisa Schink>
2017-01-24 16:39:29 -0500

    stores keys in variable

--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
-var keys = require('keys.js');
+var keys = require('./keys.js');
+
+var keysData = [];
+
+for (key in keys.twitterKeys){
+  keysData.push(keys.twitterKeys[key]);
+}
+
+console.log(keysData);
